496fd65d1b9edaaa40645f3da9e069c3e0ec03bc
DAVYDENKO OLEH <oleg.davudenko3817@gmail.com>
2026-06-15 18:38:01 +0300

    Update print_project_prices.sql

--- print_project_prices.sql
+++ print_project_prices.sql
@@ -1,13 +1,14 @@
-SELECT 
-	p.id,
-	SUM(w.salary) * DATEDIFF('MONTH, p.start_date, p.finish_date) AS price
+SELECT
+    p.name,
+    SUM(w.salary) * DATEDIFF('MONTH', p.start_date, p.finish_date) AS price
 FROM project p
 JOIN project_worker pw
- 	ON p.id = pw.project_id
+    ON p.id = pw.project_id
 JOIN worker w
-	ON pw.worker_id = w.id
+    ON pw.worker_id = w.id
 GROUP BY
-	p.id,
-	p_start_date
-	p_finish_date
-ORDER BY price DESC
+    p.id,
+    p.name,
+    p.start_date,
+    p.finish_date
+ORDER BY price DESC;
